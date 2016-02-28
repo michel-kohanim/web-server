8e7737a2da581e720b8dfafe79a39dcdf315b9e3
MKohanim <michel.kohanim@gmai.com>
2016-02-28 15:38:38 -0800

    Added middleware.js

---
+++ middleware.js
@@ -0,0 +1,12 @@
+module.exports = {
+	requireAuthentication: function(req, res, next)
+	{
+		console.log('Private route hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log(new Date().toString() + ' : - Request:' + req.method + ' ' + req.originalUrl); 
+		next();
+	}
+};
+
